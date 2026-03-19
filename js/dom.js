e672b89aa684e819948f31667975053625b9b7b5
harsh-999bit <h2821478@gmail.com>
2026-03-19 10:19:30 +0530

    functionJS

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,9 @@
-function result(){
-    document.getElementById("text").style.color="blue"
-    document.getElementsByTagName('body')[0].style.backgroundColor="black"
+function result() {
+    document.getElementById("text").style.color = "beige";
+    document.getElementsByTagName('body')[0].style.backgroundColor = "black";
+    document.getElementById("text").style.textDecoration = "underline";
+    document.getElementById("text").style.textAlign = "center";
+     document.getElementById("para").style.color="yellow";
+    document.getElementById("para").style.textAlign = "center";
 }
 
