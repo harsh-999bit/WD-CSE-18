0f3aeb465b459f9a9664eee89b8134f07475298a
harsh-999bit <h2821478@gmail.com>
2026-04-13 09:02:49 +0530

    practical

--- js/dom.js
+++ js/dom.js
@@ -3,7 +3,5 @@ function result() {
     document.getElementsByTagName('body')[0].style.backgroundColor = "black";
     document.getElementById("text").style.textDecoration = "underline";
     document.getElementById("text").style.textAlign = "center";
-     document.getElementById("para").style.color="yellow";
-    document.getElementById("para").style.textAlign = "center";
+   
 }
-
